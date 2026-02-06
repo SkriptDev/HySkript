7867d783e50a1b63f99075421cb28d2ec83975e0
ShaneBeee <shanebolenback@me.com>
2026-02-06 08:56:36 -0800

    SkriptRegistration - change enum registration

--- src/main/java/com/github/skriptdev/skript/api/skript/registration/SkriptRegistration.java
+++ src/main/java/com/github/skriptdev/skript/api/skript/registration/SkriptRegistration.java
@@ -75,14 +75,15 @@ public class SkriptRegistration extends io.github.syst3ms.skriptparser.registrat
         private final DefaultAssetMap<K, C> assetMap; // Store for later use?!?!
         private final Map<String, C> assetStoreValues = new TreeMap<>();
 
-        public AssetStoreRegistrar(Class<C> assetClass, DefaultAssetMap<K, C> assetMap, String name, String pattern) {
+        public AssetStoreRegistrar(Class<C> assetClass, DefaultAssetMap<K, C> assetMap, String baseName, String pattern) {
             this.assetClass = assetClass;
-            this.baseName = name;
+            this.baseName = baseName;
             this.pattern = pattern;
             this.assetMap = assetMap;
             assetMap.getAssetMap().forEach((key, value) -> this.assetStoreValues.put(key.toLowerCase(Locale.ROOT), value));
             this.supplier = () -> assetStoreValues.values().stream().iterator();
             this.literalParser = s -> this.assetStoreValues.get(s.toLowerCase(Locale.ROOT).replace(" ", "_"));
+
         }
 
         /**
@@ -178,6 +179,152 @@ public class SkriptRegistration extends io.github.syst3ms.skriptparser.registrat
                               Function<? super C, String> toStringFunction, Changer<? super C> defaultChanger,
                               Arithmetic<C, ?> arithmetic, Documentation documentation, TypeSerializer<C> serializer,
                               Supplier<Iterator<C>> supplier) {
+            super(c, baseName, pattern, literalParser, toStringFunction, defaultChanger,
+                arithmetic, documentation, serializer, supplier);
+        }
+    }
+
+    /**
+     * Create a new Enum type.
+     *
+     * @param enumClass Class of the Enum
+     * @param baseName  Name of the Enum type
+     * @param pattern   Pattern for the Enum type
+     * @param <E>       Type parameter for the Enum
+     * @return EnumRegistrar for configuring the Enum type
+     */
+    public <E extends Enum<E>> EnumRegistrar<E> newEnumType(Class<E> enumClass, String baseName, String pattern) {
+        return new EnumRegistrar<>(enumClass, baseName, pattern);
+    }
+
+    public class EnumRegistrar<E extends Enum<E>> {
+        private final Class<E> enumClass;
+        private final String baseName;
+        private final String pattern;
+
+        private Function<? super E, String> toStringFunction;
+        private final Function<String, ? extends E> literalParser;
+        @Nullable
+        private Changer<? super E> defaultChanger;
+        @Nullable
+        private Arithmetic<E, ?> arithmetic;
+        @Nullable
+        private TypeSerializer<E> serializer;
+        private final Supplier<Iterator<E>> supplier;
+        private final Documentation documentation = new Documentation();
+        private final Map<String, E> values = new TreeMap<>();
+
+        public EnumRegistrar(Class<E> enumClass, String name, String pattern) {
+            for (E e : enumClass.getEnumConstants()) {
+                this.values.put(e.name().toLowerCase(Locale.ROOT), e);
+            }
+            this.enumClass = enumClass;
+            this.baseName = name;
+            this.pattern = pattern;
+            this.documentation.setUsage(String.join(", ", this.values.keySet()));
+            this.supplier = () -> this.values.values().iterator();
+            this.literalParser = s -> this.values.get(s.toLowerCase(Locale.ROOT).replace(" ", "_"));
+            this.toStringFunction = e -> e.name().toLowerCase(Locale.ROOT);
+        }
+
+
+        /**
+         * @param toStringFunction a function converting an instance of the type to a String
+         * @return the registrar
+         */
+        public EnumRegistrar<E> toStringFunction(Function<? super E, String> toStringFunction) {
+            this.toStringFunction = c -> c == null ? TypeManager.NULL_REPRESENTATION : toStringFunction.apply(c);
+            return this;
+        }
+
+        /**
+         * @param defaultChanger a default {@link Changer} for this type
+         * @return the registrar
+         */
+        public EnumRegistrar<E> defaultChanger(Changer<? super E> defaultChanger) {
+            this.defaultChanger = defaultChanger;
+            return this;
+        }
+
+        /**
+         * @param arithmetic a default {@link Arithmetic} for this type
+         * @return the registrar
+         */
+        public <R> EnumRegistrar<E> arithmetic(Arithmetic<E, R> arithmetic) {
+            this.arithmetic = arithmetic;
+            return this;
+        }
+
+        /**
+         * @param serializer add a type serializer that allows the type to be saved to databases.
+         * @return the registrar
+         */
+        public EnumRegistrar<E> serializer(TypeSerializer<E> serializer) {
+            this.serializer = serializer;
+            return this;
+        }
+
+        /**
+         * Prevent docs from being printed.
+         *
+         * @return the registrar
+         */
+        public <R> EnumRegistrar<E> noDoc() {
+            this.documentation.noDoc();
+            return this;
+        }
+
+        public <R> EnumRegistrar<E> experimental() {
+            this.documentation.experimental();
+            return this;
+        }
+
+        public <R> EnumRegistrar<E> experimental(String message) {
+            this.documentation.experimental(message);
+            return this;
+        }
+
+        public <R> EnumRegistrar<E> name(String name) {
+            this.documentation.setName(name);
+            return this;
+        }
+
+        public <R> EnumRegistrar<E> description(String... description) {
+            this.documentation.setDescription(description);
+            return this;
+        }
+
+        public <R> EnumRegistrar<E> examples(String... examples) {
+            this.documentation.setExamples(examples);
+            return this;
+        }
+
+        public <R> EnumRegistrar<E> usage(String usage) {
+            this.documentation.setUsage(usage);
+            return this;
+        }
+
+        public <R> EnumRegistrar<E> since(String since) {
+            this.documentation.setSince(since);
+            return this;
+        }
+
+        /**
+         * Adds this type to the list of currently registered syntaxes
+         */
+        public void register() {
+            EnumType<E> enumType = new EnumType<>(this.enumClass, baseName, pattern, literalParser, toStringFunction,
+                defaultChanger, arithmetic, documentation, serializer, this.supplier);
+            newTypes = true;
+            types.add(enumType);
+        }
+    }
+
+    public static class EnumType<E extends Enum<E>> extends Type<E> {
+        public EnumType(Class<E> c, String baseName, String pattern, Function<String, ? extends E> literalParser,
+                        Function<? super E, String> toStringFunction, Changer<? super E> defaultChanger,
+                        Arithmetic<E, ?> arithmetic, Documentation documentation, TypeSerializer<E> serializer,
+                        Supplier<Iterator<E>> supplier) {
             super(c, baseName, pattern, literalParser, toStringFunction, defaultChanger,
                 arithmetic, documentation, serializer, supplier);
         }
