703893d64894e71355fbd6d30898632591750771
ShaneBeee <shanebolenback@me.com>
2026-02-11 18:33:01 -0800

    TestRunner - more changes

--- src/main/java/com/github/skriptdev/skript/api/skript/testing/TestRunner.java
+++ src/main/java/com/github/skriptdev/skript/api/skript/testing/TestRunner.java
@@ -9,11 +9,13 @@ import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.util.MessageUtil;
 import fi.sulku.hytale.TinyMsg;
 import io.github.syst3ms.skriptparser.lang.Statement;
+import io.github.syst3ms.skriptparser.lang.Trigger;
 import io.github.syst3ms.skriptparser.lang.TriggerMap;
 import io.github.syst3ms.skriptparser.log.LogEntry;
 import io.github.syst3ms.skriptparser.log.LogType;
 import io.github.syst3ms.skriptparser.parsing.ScriptLoader;
 import io.github.syst3ms.skriptparser.registration.SkriptAddon;
+import io.github.syst3ms.skriptparser.variables.Variables;
 
 import java.io.File;
 import java.nio.file.Path;
@@ -21,10 +23,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class TestRunner {
 
+    private static final ScheduledExecutorService SCHEDULED = HytaleServer.SCHEDULED_EXECUTOR;
     private final TestResults testResults = new TestResults();
     private final World world = Universe.get().getWorld("default");
 
@@ -40,13 +44,15 @@ public class TestRunner {
             loadTests();
             Utils.log("Finished loading test scripts!");
 
+            // Make sure the world isn't paused
             if (this.world.isPaused()) this.world.setPaused(false);
 
+            // Run our tests in the world to make sure we have access to blocks/entities
             this.world.execute(runTestsRunnable);
         };
 
         // Delay start to make sure the server has finished loading
-        HytaleServer.SCHEDULED_EXECUTOR.schedule(loadTestsRunnable, 1, TimeUnit.SECONDS);
+        SCHEDULED.schedule(loadTestsRunnable, 1, TimeUnit.SECONDS);
     }
 
     private void loadTests() {
@@ -55,38 +61,45 @@ public class TestRunner {
     }
 
     private void runTests() {
-        TestContext testContext = new TestContext(this.testResults, this.world);
-
         // Catch exceptions and treat them as failures
         Statement.setExceptionHandler(e ->
             this.testResults.addFailure("Exception",
                 e.getClass().getSimpleName() + ": " + e.getMessage()));
 
         // Run all the test triggers
-        TriggerMap.callTriggersByContext(testContext);
-
-        // Process results
-        this.testResults.process();
-
-        // Print results
-        if (this.testResults.isSuccess()) {
-            Message message = TinyMsg.parse("<green>All tests passed!");
-            Utils.log(MessageUtil.toAnsiString(message).toAnsi());
-        } else {
-            Utils.error(this.testResults.getFailCount() + " tests failed!");
-            this.testResults.getFailureMap().forEach((test, failure) ->
-                Utils.error(" - [" + test + "]: " + failure));
+        for (Trigger allTrigger : TriggerMap.getAllTriggers()) {
+            TestContext context = new TestContext(this.testResults, this.world);
+            Statement.runAll(allTrigger, context);
+            Variables.clearLocalVariables(context);
         }
 
-        Utils.log("Finished running tests!");
+        Runnable finishingRunnable = () -> {
+            // Process results
+            this.testResults.process();
 
-        // Print results to file
-        this.testResults.printToJsonFile();
-        this.testResults.clear();
+            // Print results
+            if (this.testResults.isSuccess()) {
+                Message message = TinyMsg.parse("<green>All tests passed!");
+                Utils.log(MessageUtil.toAnsiString(message).toAnsi());
+            } else {
+                Utils.error(this.testResults.getFailCount() + " tests failed!");
+                this.testResults.getFailureMap().forEach((test, failure) ->
+                    Utils.error(" - [" + test + "]: " + failure));
+            }
 
-        // Shutdown server
-        Runnable shutdownServer = () -> HytaleServer.get().shutdownServer();
-        HytaleServer.SCHEDULED_EXECUTOR.schedule(shutdownServer, 2, TimeUnit.SECONDS);
+            Utils.log("Finished running tests!");
+
+            // Print results to file
+            this.testResults.printToJsonFile();
+            this.testResults.clear();
+
+            // Shutdown server
+            // Give it a bit of delay to really make sure we're finished
+            SCHEDULED.schedule(() -> HytaleServer.get().shutdownServer(), 1, TimeUnit.SECONDS);
+        };
+
+        // Delay finish to allow waits in tests
+        SCHEDULED.schedule(finishingRunnable, 1, TimeUnit.SECONDS);
     }
 
     private void loadScripts(Path directory) {
