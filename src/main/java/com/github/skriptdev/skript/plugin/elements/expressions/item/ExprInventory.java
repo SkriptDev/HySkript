a9033655c2631b1fc70a5c1273f9865df893f20c
ShaneBeee <shanebolenback@me.com>
2026-03-06 17:31:13 -0800

    ExprInventory - change again

--- src/main/java/com/github/skriptdev/skript/plugin/elements/expressions/item/ExprInventory.java
+++ src/main/java/com/github/skriptdev/skript/plugin/elements/expressions/item/ExprInventory.java
@@ -1,9 +1,16 @@
 package com.github.skriptdev.skript.plugin.elements.expressions.item;
 
+import com.github.skriptdev.skript.api.hytale.utils.EntityUtils;
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.server.core.entity.LivingEntity;
 import com.hypixel.hytale.server.core.inventory.InventoryComponent;
+import com.hypixel.hytale.server.core.inventory.InventoryComponent.Armor;
+import com.hypixel.hytale.server.core.inventory.InventoryComponent.Backpack;
+import com.hypixel.hytale.server.core.inventory.InventoryComponent.Hotbar;
+import com.hypixel.hytale.server.core.inventory.InventoryComponent.Storage;
+import com.hypixel.hytale.server.core.inventory.InventoryComponent.Tool;
+import com.hypixel.hytale.server.core.inventory.InventoryComponent.Utility;
 import com.hypixel.hytale.server.core.inventory.ItemStack;
 import com.hypixel.hytale.server.core.inventory.container.CombinedItemContainer;
 import com.hypixel.hytale.server.core.inventory.container.ItemContainer;
@@ -26,8 +33,9 @@ public class ExprInventory implements Expression<ItemContainer> {
                 "inventory of %livingentities%")
             .name("Inventory of LivingEntity")
             .description("While it is called \"inventory\" it will " +
-                    "actually return the combined everything ItemContainer of a LivingEntity.",
-                "This is essentially a shortcut for the `combined everything item contatiner of` expression.")
+                    "actually return the combined hotbar/storage ItemContainers of a LivingEntity.",
+                "This is essentially a shortcut for the `combined hotbar first item contatiner of` expression.",
+                "Clearing will clear all ItemContainers, not just the hotbar/storage.")
             .examples("set {_inv} to inventory of player",
                 "clear inventory of player",
                 "add itemstack of ingredient_stick to inventory of player")
@@ -35,24 +43,24 @@ public class ExprInventory implements Expression<ItemContainer> {
             .register();
     }
 
-    private Expression<LivingEntity> entity;
+    private Expression<LivingEntity> entities;
 
     @SuppressWarnings("unchecked")
     @Override
     public boolean init(Expression<?>[] expressions, int matchedPattern, @NotNull ParseContext parseContext) {
-        this.entity = (Expression<LivingEntity>) expressions[0];
+        this.entities = (Expression<LivingEntity>) expressions[0];
         return true;
     }
 
     @Override
     public ItemContainer[] getValues(@NotNull TriggerContext ctx) {
         List<ItemContainer> containers = new ArrayList<>();
-        for (LivingEntity livingEntity : this.entity.getArray(ctx)) {
-            Ref<EntityStore> reference = livingEntity.getReference();
-            if (reference == null) continue;
+        for (LivingEntity livingEntity : this.entities.getArray(ctx)) {
+            Ref<EntityStore> ref = livingEntity.getReference();
+            if (ref == null) continue;
 
-            Store<EntityStore> store = reference.getStore();
-            CombinedItemContainer combined = InventoryComponent.getCombined(store, reference, InventoryComponent.EVERYTHING);
+            Store<EntityStore> store = ref.getStore();
+            CombinedItemContainer combined = InventoryComponent.getCombined(store, ref, InventoryComponent.HOTBAR_FIRST);
             containers.add(combined);
         }
         return containers.toArray(new ItemContainer[0]);
@@ -70,16 +78,40 @@ public class ExprInventory implements Expression<ItemContainer> {
     public void change(@NotNull TriggerContext ctx, @NotNull ChangeMode changeMode, Object @NotNull [] changeWith) {
         ItemContainer[] toChange = getValues(ctx);
         if (changeMode == ChangeMode.ADD) {
-            for (ItemContainer inventory : toChange) {
+            for (ItemContainer container : toChange) {
                 for (Object o : changeWith) {
                     if (o instanceof ItemStack itemStack) {
-                        inventory.addItemStack(itemStack);
+                        container.addItemStack(itemStack);
                     }
                 }
             }
         } else if (changeMode == ChangeMode.DELETE) {
-            for (ItemContainer inventory : toChange) {
-                inventory.clear();
+            for (LivingEntity livingEntity : this.entities.getArray(ctx)) {
+                // Combined storage clearing is currently broken
+                Storage storage = EntityUtils.getComponent(livingEntity, Storage.getComponentType());
+                if (storage != null) {
+                    storage.getInventory().clear();
+                }
+                Hotbar hotbar = EntityUtils.getComponent(livingEntity, Hotbar.getComponentType());
+                if (hotbar != null) {
+                    hotbar.getInventory().clear();
+                }
+                Armor armor = EntityUtils.getComponent(livingEntity, Armor.getComponentType());
+                if (armor != null) {
+                    armor.getInventory().clear();
+                }
+                Backpack backpack = EntityUtils.getComponent(livingEntity, Backpack.getComponentType());
+                if (backpack != null) {
+                    backpack.getInventory().clear();
+                }
+                Utility utility = EntityUtils.getComponent(livingEntity, Utility.getComponentType());
+                if (utility != null) {
+                    utility.getInventory().clear();
+                }
+                Tool tool = EntityUtils.getComponent(livingEntity, Tool.getComponentType());
+                if (tool != null) {
+                    tool.getInventory().clear();
+                }
             }
         }
     }
@@ -91,7 +123,7 @@ public class ExprInventory implements Expression<ItemContainer> {
 
     @Override
     public String toString(@NotNull TriggerContext ctx, boolean debug) {
-        return "inventory of " + this.entity.toString(ctx, debug);
+        return "inventory of " + this.entities.toString(ctx, debug);
     }
 
 }
