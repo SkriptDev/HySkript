98fc685e7a19878878c08aec6103088a48e860c9
ShaneBeee <shanebolenback@me.com>
2026-03-08 09:22:28 -0700

    ExprWorldTimeDurations - add changers

--- src/main/java/com/github/skriptdev/skript/plugin/elements/expressions/world/ExprWorldTimeDurations.java
+++ src/main/java/com/github/skriptdev/skript/plugin/elements/expressions/world/ExprWorldTimeDurations.java
@@ -1,14 +1,19 @@
 package com.github.skriptdev.skript.plugin.elements.expressions.world;
 
 import com.github.skriptdev.skript.api.skript.registration.SkriptRegistration;
+import com.github.skriptdev.skript.api.utils.ReflectionUtils;
 import com.hypixel.hytale.server.core.universe.world.World;
 import io.github.syst3ms.skriptparser.lang.Expression;
+import io.github.syst3ms.skriptparser.lang.TriggerContext;
 import io.github.syst3ms.skriptparser.lang.properties.PropertyExpression;
 import io.github.syst3ms.skriptparser.parsing.ParseContext;
+import io.github.syst3ms.skriptparser.types.changers.ChangeMode;
+import io.github.syst3ms.skriptparser.util.CollectionUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.time.Duration;
+import java.util.Optional;
 
 public class ExprWorldTimeDurations extends PropertyExpression<World, Duration> {
 
@@ -18,8 +23,11 @@ public class ExprWorldTimeDurations extends PropertyExpression<World, Duration> 
             .name("World Time Durations")
             .description("Represents the daytime/nighttime durations of a world.",
                 "These values are from a GamePlayConfig but can be overridden in your World config.",
-                "**Note**: Currently these cannot be set via code.")
-            .examples("set {_Day} to world daytime duration of world of player")
+                "Set/add/remove/reset will update your world config with these values.")
+            .examples("set {_Day} to world daytime duration of world of player",
+                "set world daytime duration of {_world} to 1 hour",
+                "add 10 minutes to world daytime duration of {_world}",
+                "reset world daytime duration of {_world}")
             .since("1.1.0")
             .register();
     }
@@ -41,6 +49,55 @@ public class ExprWorldTimeDurations extends PropertyExpression<World, Duration> 
             default -> 0;
         };
         return Duration.ofSeconds(seconds);
+    }
+
+    @Override
+    public Optional<Class<?>[]> acceptsChange(ChangeMode mode) {
+        return switch (mode) {
+            case ADD, REMOVE, SET, RESET -> {
+                if (this.pattern == 2) {
+                    // LOG
+                    yield Optional.empty();
+                }
+                yield CollectionUtils.optionalArrayOf(Duration.class);
+            }
+            default -> Optional.empty();
+        };
+    }
+
+    @SuppressWarnings("ConstantValue")
+    @Override
+    public void change(@NotNull TriggerContext ctx, @NotNull ChangeMode changeMode, Object @NotNull [] changeWith) {
+        Duration duration = null;
+        if (changeWith != null && changeWith.length > 0 && changeWith[0] instanceof Duration d) {
+            duration = d;
+        }
+
+        int durationSeconds;
+        if (duration == null) {
+            durationSeconds = 0;
+        } else {
+            durationSeconds = (int) duration.toMillis() / 1000;
+        }
+
+        boolean daytime = this.pattern == 0;
+        for (World world : getOwner().getArray(ctx)) {
+            int previons;
+            if (daytime) {
+                previons = world.getDaytimeDurationSeconds();
+            } else {
+                previons = world.getNighttimeDurationSeconds();
+            }
+
+            Integer seconds = switch (changeMode) {
+                case ADD -> previons + durationSeconds;
+                case REMOVE -> previons - durationSeconds;
+                case SET -> durationSeconds;
+                default -> null;
+            };
+
+            ReflectionUtils.setWorldTimeOverrides(world, seconds, daytime);
+        }
     }
 
 }
