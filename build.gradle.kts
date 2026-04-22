c6dcae2c8e7b0ff22a6f5f7dc3256847e728a844
ShaneBeee <shanebolenback@me.com>
2026-04-22 15:50:35 -0700

    build.gradle.kts - update pre-release

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ java.sourceCompatibility = JavaVersion.VERSION_25
 
 group = "com.github.SkriptDev"
 val projectVersion = "1.6.0"
-val hytaleVersion = "2026.04.16-5cbd1e160"
+val hytaleVersion = "2026.04.17-c2d518cc9"
 // You can find Hytale versions on their maven repo:
 // https://maven.hytale.com/release/com/hypixel/hytale/Server/maven-metadata.xml
 // https://maven.hytale.com/pre-release/com/hypixel/hytale/Server/maven-metadata.xml
