5484986fa6721d01a4d369b6e0e0d7c7aa3a6be7
ShaneBeee <shanebolenback@me.com>
2026-05-26 07:23:38 -0700

    Prepare for release of 1.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 java.sourceCompatibility = JavaVersion.VERSION_25
 
 group = "com.github.SkriptDev"
-val projectVersion = "1.6.0"
+val projectVersion = "1.7.0"
 val hytaleVersion = "0.5.0"
 // You can find Hytale versions on their maven repo:
 // https://maven.hytale.com/release/com/hypixel/hytale/Server/maven-metadata.xml
